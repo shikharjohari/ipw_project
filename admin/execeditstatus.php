212cd399d4c66be0f7c14b263f41f76adab52c7b
shikharjohari <shikhar.johari2014@vit.ac.in>
2017-05-04 23:15:40 +0530

    Revert "Final Commit"
    This reverts commit 06b4119e0cd0cd4d6811adb0f355903ff8966244.

--- admin/execeditstatus.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include('../store/connect.php');
-	$roomid = $_POST['roomid'];
-	$status=$_POST['status'];
-	mysql_query("UPDATE reservation SET status='$status' WHERE reservation_id='$roomid'");
-	header("location: index.php");
-?>
